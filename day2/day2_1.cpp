6607eaa17d8b9a6a3492cc24a329ee47c5920b93
tomas1978 <tomas.nilsson78@gmail.com>
2022-12-07 22:49:01 +0100

    Some test outputs

--- day2/day2_1.cpp
+++ day2/day2_1.cpp
@@ -10,14 +10,14 @@ int main()
   string line;
   vector<string> gameInputs;
 
-  //Calculate calory sum of each elf, store in vector
   while(getline(infile, line)) 
-    {
-      if(line.empty()) 
-	{
-	  gameInputs.push_back(line);
-	}
-    }
-  for(int i=0;i<10;i++)
-    cout<<gameInputs.at(i)<<endl;
+  {
+    cout<<line<<endl;
+    if(line.empty()) 
+	  {
+	    gameInputs.push_back(line);
+  	}
+    for(int i=0;i<10;i++)
+      cout<<gameInputs.at(i)<<endl;
+  }
 }
