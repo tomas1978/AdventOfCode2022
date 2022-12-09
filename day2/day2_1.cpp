5e1647c3029555c132f56a604336097f2c3bdb6c
tomas1978 <tomas.nilsson78@gmail.com>
2022-12-09 08:16:38 +0100

    Started implementing evaulation function

--- day2/day2_1.cpp
+++ day2/day2_1.cpp
@@ -4,6 +4,24 @@
 #include<vector>
 using namespace std;
 
+int evaulateGame(string game) 
+{
+	int score;
+	char player=game.at(0);
+	char opponent=game.at(2);
+	if(player=='A') 
+	{
+		score=1;
+		if(opponent=='A')
+			score+=3;
+		if(opponent=='B')
+			score+=0;
+		if(opponent=='C')
+			score+=6;
+	}
+	return score;
+}
+
 int main()
 {
   ifstream infile("input.txt");
@@ -12,12 +30,15 @@ int main()
 
   while(getline(infile, line)) 
   {
-   // cout<<line<<endl;
     if(!line.empty()) 
 	{
 	    gameInputs.push_back(line);
   	}
-	
   }
-  cout<<gameInputs.size()<<endl;
+  int totalScore=0;
+  for(string s:gameInputs)
+  {
+	totalScore+=evaulateGame(s);
+  }
+  cout<<"Total score: "<<totalScore<<endl;
 }
