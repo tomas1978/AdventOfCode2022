569ed996b7feacce813e0a0225e62419c7da9d95
tomas1978 <tomas.nilsson78@gmail.com>
2022-12-10 10:02:48 +0100

    First version of complete eval function *BUG*
    Implemented all cases, but does not pass any of the three tests

--- day2/day2_1.cpp
+++ day2/day2_1.cpp
@@ -9,15 +9,36 @@ int evaulateGame(string game)
 	int score;
 	char player=game.at(0);
 	char opponent=game.at(2);
-	if(player=='A') 
+
+	if(player=='A') //Player chooses rock
 	{
 		score=1;
-		if(opponent=='A')
+		if(opponent=='X') //Opponent chooses rock
 			score+=3;
-		if(opponent=='B')
+		if(opponent=='Y') //Opponent chooses paper
 			score+=0;
-		if(opponent=='C')
+		if(opponent=='Z') //Opponent chooses scissors
 			score+=6;
+	}
+	if(player=='B') //Player chooses paper
+	{
+		score=2;
+		if(opponent=='X')
+			score+=6;
+		if(opponent=='Y')
+			score+=3;
+		if(opponent=='Z')
+			score+=0;
+	}
+	else //Player chooses scissors
+	{
+		score=3;
+		if(opponent=='X')
+			score+=0;
+		if(opponent=='Y')
+			score+=6;
+		if(opponent=='Z')
+			score+=0;
 	}
 	return score;
 }
@@ -41,4 +62,7 @@ int main()
 	totalScore+=evaulateGame(s);
   }
   cout<<"Total score: "<<totalScore<<endl;
+  cout<<"TESTING"<<endl;
+  string test="C Z";
+  cout<<"SCore: "<<evaulateGame(test)<<endl;
 }
