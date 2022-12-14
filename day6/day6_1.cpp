0e602f7cae62b71421ed1ae2817257133e688ff5
tomas1978 <tomas.nilsson78@gmail.com>
2022-12-14 10:58:18 +0100

    Writing on function to find marker
    Tried to implement function, unfortunately it does not give the right answer

--- day6/day6_1.cpp
+++ day6/day6_1.cpp
@@ -6,18 +6,26 @@ using namespace std;
 
 int findStartOfPacket(string buffer) 
 {
-	return 0;
+	int markerPosition = -1;
+	for(int i=0;i<buffer.size()-4;i++)
+	{
+		if(buffer.at(i)==buffer.at(i+1) && buffer.at(i+1)==buffer.at(i+2) &&
+			buffer.at(i+2)==buffer.at(i+3))
+			markerPosition=i;
+	}
+	return markerPosition;
 }
 
 int main()
 {
   ifstream infile("input.txt");
   string line, buffer;
-  vector<string> gameInputs;
-
+  
   while(getline(infile, line)) //Read from file, put characters in buffer 
   {
 		buffer=line;
   }
-  cout<<buffer<<endl;
+  
+  int markerPos = findStartOfPacket(buffer);
+  cout<<"Start of packet marker position: "<<markerPos<<endl;
 }
